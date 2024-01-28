206fb46063e96245bf6ded65b437a5ca9da379db
Sawozz <cavva45@gmail.com>
2024-01-28 19:06:32 +0300

    123
    123

--- cs/Job.cs
+++ cs/Job.cs
@@ -1,6 +1,29 @@
 using System;
-class HelloWorld {
-  static void Main() {
-    Console.WriteLine("Hello World");
-  }
+
+namespace NumberComparison
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите два числа");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            if (a > b)
+            {
+                Console.WriteLine($"Первое число ({a}) больше второго ({b})");
+            }
+            else if (a < b)
+            {
+                Console.WriteLine($"Первое число ({a}) меньше второго ({b})");
+            
+            }
+
+            else
+            {
+                 Console.WriteLine("Числа равны");
+            }
+        }
+    }
 }
